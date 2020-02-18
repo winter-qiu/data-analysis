6f2cd9e3456b3a9ffebac080bbd63ed260e0ef94
DEOLAY <dong.qiu@mail.concordia.ca>
2020-02-18 12:28:02 -0500

    update

--- 1. Import Data/read_local_file.py
+++ 1. Import Data/read_local_file.py
@@ -1,19 +1,22 @@
 # Python has in-built functions to create and manipulate files. No need to import.
+
+##############################################################################
+# Read an txt file
 # Import an file object 
 my_file = open("myLocalFile.txt", mode="a+", encoding="utf-8")
 
-#Character	Function
-# r	- Open file for reading only. Starts reading from beginning of file.
-#rb	- Open a file for reading only in binary format.
-#r+	- Open file for reading and writing.
-# w	- Open file for writing only.
-#wb	- Same as 'w' but opens in binary mode.
-#w+	- Same as 'w' but also alows to read from file.
-#wb+ - Same as 'wb' but also alows to read from file.
-# a	- Open a file for appending. Starts writing at the end of file.
-#ab	- Same as 'a' but in binary format.
-#a+ - Same a 'a' but also open for reading.
-#ab+ - Same a 'ab' but also open for reading.
+#Character: Function
+# Useful ones 
+#r: Open file for reading only. Starts reading from beginning of file.
+#w: Open file for writing only.
+#a: Open a file for appending. Starts writing at the end of file.
+
+# Less useful ones 
+#r+: Open file for reading and writing.
+#w+: Same as 'w' but also alows to read from file.
+#wb+: Same as 'wb' but also alows to read from file.
+#a+: Same a 'a' but also open for reading.
+#ab+: Same a 'ab' but also open for reading.
 
 # Read the whole file
 file_content = my_file.read()
@@ -30,3 +33,31 @@ line2 = my_file.readline()
 my_file.close()
 
 ##############################################################################
+# Read a csv file
+# Read the whole file
+file_content = my_file.read()
+
+# Read the each line of the object
+for line in my_file:
+    print(line)
+
+# or 
+line1 = my_file.readline()
+line2 = my_file.readline()
+
+# Close the file object
+my_file.close()
+
+
+##############################################################################
+# Read a JSON file
+import json
+
+input_file = "json_file.json"
+# regular JSON format
+with open(input_file,"r") as f:
+    jsondata=json.load(f)
+
+# NJSON format
+for line in open(input_file):
+    line = eval(line)
