6cee8ec39401acf01e30c6c68af6f6771064d777
DEOLAY <dong.qiu@mail.concordia.ca>
2020-02-18 12:20:28 -0500

    update

--- 1. Import Data/read_local_file.py
+++ 1. Import Data/read_local_file.py
@@ -3,17 +3,17 @@
 my_file = open("myLocalFile.txt", mode="a+", encoding="utf-8")
 
 #Character	Function
-# r	        Open file for reading only. Starts reading from beginning of file.
-#rb	        Open a file for reading only in binary format. 
-#r+	        Open file for reading and writing. 
-# w	        Open file for writing only. 
-#wb	        Same as 'w' but opens in binary mode.
-#w+	        Same as 'w' but also alows to read from file.
-#wb+	    Same as 'wb' but also alows to read from file.
-#a	        Open a file for appending. Starts writing at the end of file. 
-#ab	        Same as 'a' but in binary format. 
-#a+	        Same a 'a' but also open for reading.
-#ab+	    Same a 'ab' but also open for reading.
+# r	 - Open file for reading only. Starts reading from beginning of file.
+#rb	 - Open a file for reading only in binary format. 
+#r+	 - Open file for reading and writing. 
+# w	 - Open file for writing only. 
+#wb	 - Same as 'w' but opens in binary mode.
+#w+	 - Same as 'w' but also alows to read from file.
+#wb+ - Same as 'wb' but also alows to read from file.
+# a	 - Open a file for appending. Starts writing at the end of file. 
+#ab	 - Same as 'a' but in binary format. 
+#a+  - Same a 'a' but also open for reading.
+#ab+ - Same a 'ab' but also open for reading.
 
 # Read the whole file
 file_content = my_file.read()
